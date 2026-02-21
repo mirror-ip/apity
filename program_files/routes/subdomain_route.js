847276840bcb5a119712a4f3826e40a1ad2c2c82
mirror-ip <leduchoanghai@yahoo.com>
2026-02-21 21:56:00 +0700

    Update subdomain_route.js

--- program_files/routes/subdomain_route.js
+++ program_files/routes/subdomain_route.js
@@ -1,3 +1,11 @@
+/**
+ * APITY: APITY Web Sevices.
+ *
+ * @copyright Copyright (c) 2026, Hoàng Hải <leduchoanghai@yahoo.com>
+ * @license   MIT, http://www.opensource.org/licenses/mit-license.php
+ */
+
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
